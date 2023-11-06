e3ca2498197ea11f5d8a84657d95518174ab0f9a
longdream12 <149510741+longdream12@users.noreply.github.com>
2023-11-06 15:35:59 -0600

    Add files via upload

--- flowerscript.js
+++ flowerscript.js
@@ -1,15 +1,10 @@
 const image = document.getElementById("image");
-const clickWord = document.getElementById("click-word");
 const videoPopup = document.getElementById("video-popup");
 const video = document.getElementById("video");
 const closeButton = document.getElementById("close-button");
 
-const imageSources = ["mainlobby.jpg", "gym.jpg", "cafe2.0.jpg", "flowerinsand.jpeg", "engineer.jpeg", "hiking.jpeg", "auditorium.jpg", "draw.jpeg", "plane.jpeg", "sidelobby", "space zero.jpeg", "window.jpeg"]; // Add your image sources here
-let currentIndex = 0;
-
-clickWord.addEventListener("click", () => {
+image.addEventListener("click", () => {
     videoPopup.style.display = "block";
-    video.play();
 });
 
 closeButton.addEventListener("click", () => {
@@ -24,13 +19,4 @@ window.addEventListener("click", (event) => {
     }
 });
 
-function changeImage() {
-    currentIndex = (currentIndex + 1) % imageSources.length;
-    image.src = imageSources[currentIndex];
-}
 
-// Change the image every 5 seconds (5000 milliseconds)
-setInterval(changeImage, 5000);
-
-// Initially start changing the image
-changeImage();
